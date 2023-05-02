813ec1d6ada99277dcfe50f814733a1a8ed3454c
Chokri Hammedi <52697989+blue0x1@users.noreply.github.com>
2023-05-02 13:02:08 +0100

    Update client.py

--- client.py
+++ client.py
@@ -1,15 +1,37 @@
 import zmq
 import subprocess
+import os
+import getpass
+
+print("Sisyphus - Remote Command Execution Tool")
+print("Author: Chokri Hammedi\n")
+
 
 host = '192.168.8.107'
-port = 5001
+port = 5000
 
 context = zmq.Context()
 socket = context.socket(zmq.REQ)
 socket.connect(f"tcp://{host}:{port}")
 
+
+username = getpass.getuser()
+
+
+socket.send_string("get_cwd")
+cwd = socket.recv_string()
+
 while True:
-    command = input("Enter command: ")
+    command = input(f"{username}@{cwd}> ")
+    
+    if command.lower() == "exit":
+        socket.send_string(command)
+        break
+    
     socket.send_string(command)
     output = socket.recv_string()
-    print(output)
+    
+    if command.startswith("cd "):
+        cwd = output.strip()  
+    else:
+        print(output)
